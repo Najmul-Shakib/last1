738287f7291626c9be2b2d801784bbd474c5eebf
saki <najmulshakibf@gmail.com>
2025-03-22 01:14:21 +1100

    l

---
+++ scripts/checkBox.js
@@ -0,0 +1,70 @@
+document.getElementById('btn-completed-1').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById("check-box").innerText;
+    console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal + 1;
+    console.log(valInput);
+    document.getElementById('check-box').innerHTML = `<p>${valInput}</p>`;
+
+})
+document.getElementById('btn-completed-2').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById("check-box").innerText;
+    console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal + 1;
+    console.log(valInput);
+    document.getElementById("check-box").innerHTML = `<p>${valInput}</p>`;
+
+})
+
+document.getElementById('btn-completed-3').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById("check-box").innerText;
+    console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal + 1;
+    console.log(valInput);
+    document.getElementById("check-box").innerHTML = `<p>${valInput}</p>`;
+
+})
+
+document.getElementById('btn-completed-4').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById("check-box").innerText;
+    console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal + 1;
+    console.log(valInput);
+    document.getElementById("check-box").innerHTML = `<p>${valInput}</p>`;
+
+})
+
+document.getElementById('btn-completed-5').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById("check-box").innerText;
+    console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal + 1;
+    console.log(valInput);
+    document.getElementById("check-box").innerHTML = `<p>${valInput}</p>`;
+
+})
+
+document.getElementById('btn-completed-6').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById("check-box").innerText;
+    console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal + 1;
+    console.log(valInput);
+    document.getElementById("check-box").innerHTML = `<p>${valInput}</p>`;
+
+})
