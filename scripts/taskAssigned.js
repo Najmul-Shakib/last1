b5a4289456f63c8fbf3306831ae641ae4500ac53
saki <najmulshakibf@gmail.com>
2025-03-22 00:38:12 +1100

    l

--- scripts/taskAssigned.js
+++ scripts/taskAssigned.js
@@ -1,1 +1,105 @@
-alert();
+document.getElementById('btn-completed-1').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById('task-number').innerText;
+    // console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal - 1;
+    console.log(valInput);
+    document.getElementById('task-number').innerHTML = `<p>0${valInput}</p>`;
+
+    
+    let btn = document.getElementById("btn-completed-1");
+    btn.disabled = true;
+    btn.innerText = "Completed";
+})
+
+document.getElementById('btn-completed-2').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById('task-number').innerText;
+    // console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal - 1;
+    console.log(valInput);
+    document.getElementById('task-number').innerHTML = `<p>0${valInput}</p>`;
+
+    let btn = document.getElementById("btn-completed-2");
+
+    btn.disabled = true;
+    btn.innerText = "Completed";
+
+    
+})
+
+document.getElementById('btn-completed-3').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById('task-number').innerText;
+    // console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal - 1;
+    console.log(valInput);
+    document.getElementById('task-number').innerHTML = `<p>0${valInput}</p>`;
+
+    let btn = document.getElementById("btn-completed-3");
+
+    btn.disabled = true;
+    btn.innerText = "Completed";
+
+    
+})
+
+document.getElementById('btn-completed-4').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById('task-number').innerText;
+    // console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal - 1;
+    console.log(valInput);
+    document.getElementById('task-number').innerHTML = `<p>0${valInput}</p>`;
+
+
+    let btn = document.getElementById("btn-completed-4");
+
+    btn.disabled = true;
+    btn.innerText = "Completed";
+    
+})
+
+document.getElementById('btn-completed-5').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById('task-number').innerText;
+    // console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal - 1;
+    console.log(valInput);
+    document.getElementById('task-number').innerHTML = `<p>0${valInput}</p>`;
+
+    let btn = document.getElementById("btn-completed-5");
+
+    btn.disabled = true;
+    btn.innerText = "Completed";
+
+    
+})
+
+document.getElementById('btn-completed-6').addEventListener('click', function(){
+
+    // console.log('hello');
+    const val = document.getElementById('task-number').innerText;
+    // console.log(val);
+    const newVal = parseInt(val);
+    const valInput = newVal - 1;
+    console.log(valInput);
+    document.getElementById('task-number').innerHTML = `<p>0${valInput}</p>`;
+
+    let btn = document.getElementById("btn-completed-6");
+
+    btn.disabled = true;
+    btn.innerText = "Completed";
+
+    
+})
