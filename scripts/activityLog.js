62ce6bd4cf5b9f674028970a8711aa62e6a2c311
saki <najmulshakibf@gmail.com>
2025-03-22 01:06:36 +1100

    l

---
+++ scripts/activityLog.js
@@ -0,0 +1,56 @@
+document.getElementById("btn-completed-1").addEventListener("click", function() {
+   
+    let newParagraph = document.createElement("p");
+
+   
+    newParagraph.innerHTML = "You have completed the task <br> improve job searching at 12:48:15 PM";
+
+    
+    document.getElementById("container").appendChild(newParagraph);
+});
+document.getElementById("btn-completed-2").addEventListener("click", function() {
+   
+    let newParagraph = document.createElement("p");
+
+    
+    newParagraph.innerHTML = "You have completed the task <br> improve job searching at 12:48:15 PM";
+
+    
+    document.getElementById("container").appendChild(newParagraph);
+});
+document.getElementById("btn-completed-3").addEventListener("click", function() {
+    
+    let newParagraph = document.createElement("p");
+
+    
+    newParagraph.innerHTML = "You have completed the task <br> improve job searching at 12:48:15 PM";
+    
+    document.getElementById("container").appendChild(newParagraph);
+});
+document.getElementById("btn-completed-4").addEventListener("click", function() {
+    
+    let newParagraph = document.createElement("p");
+
+    
+    newParagraph.innerHTML = "You have completed the task <br> improve job searching at 12:48:15 PM";
+    
+    document.getElementById("container").appendChild(newParagraph);
+});
+document.getElementById("btn-completed-5").addEventListener("click", function() {
+    
+    let newParagraph = document.createElement("p");
+
+    
+    newParagraph.innerHTML = "You have completed the task <br> improve job searching at 12:48:15 PM";
+    
+    document.getElementById("container").appendChild(newParagraph);
+});
+document.getElementById("btn-completed-6").addEventListener("click", function() {
+   
+    let newParagraph = document.createElement("p");
+
+    
+    newParagraph.innerHTML = "You have completed the task <br> improve job searching at 12:48:15 PM";
+   
+    document.getElementById("container").appendChild(newParagraph);
+});
